31612c1c32d454b4e74d9704f09918c1d8502c38
richplastow <rich@richplastow.com>
2015-06-02 23:58:55 +0100

    0.0.2 Minor amend to test host restart

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //// Get OpenShift variables, if available.
 //// See https://developers.openshift.com/en/node-js-project-structure.html
+var version = '0.0.2';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 1337;
 var ip   = process.env.OPENSHIFT_NODEJS_IP   || '127.0.0.1';
 
@@ -8,7 +9,7 @@ var http = require('http');
 var tally = 0;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Request tally: ' + (++tally) + '\n');
+  res.end(version + ' Request tally: ' + (++tally) + '\n');
 }).listen(port, ip);
-console.log('Server running at http://' + ip + ':' + port + '/');
+console.log(version + ' Running at http://' + ip + ':' + port + '/');
 
